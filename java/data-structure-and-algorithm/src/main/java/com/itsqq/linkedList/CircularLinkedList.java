52e4722c208b006747bd12402f35e2dc1d519501
sfqqyq <sfqqyq@163.com>
2022-03-16 14:44:49 +0800

    sfqqyq:循环链表修改只有一个元素的情况

--- java/data-structure-and-algorithm/src/main/java/com/itsqq/linkedList/CircularLinkedList.java
+++ java/data-structure-and-algorithm/src/main/java/com/itsqq/linkedList/CircularLinkedList.java
@@ -11,18 +11,18 @@ public class CircularLinkedList {
     public CircularLinkedList(){}
     public CircularLinkedList(int val){
         this.head=new Node(val,null);
-//        this.head.next=this.head;
+        this.head.next=this.head;
     }
 
     public boolean add(int val){
         if(head==null){
             this.head=new Node(val,null);
+            this.head.next=this.head;
             return true;
-//            this.head.next=this.head;
         }
 
         Node tmpN=head;
-        while (tmpN.next!=null && tmpN.next!=head){
+        while (tmpN.next!=head){
             tmpN=tmpN.next;
         }
         tmpN.next=new Node(val,head);
@@ -33,7 +33,7 @@ public class CircularLinkedList {
         Node tmpN=head;
         // 删除头
         if(head.data==val){
-            if(head.next==null) {
+            if(tmpN.next==head){// 如果只有一个元素
                 head=null;
                 return true;
             }
@@ -44,7 +44,7 @@ public class CircularLinkedList {
             head=head.next;
             return true;
         }
-        while (tmpN.next!=null && tmpN.next!=head){
+        while (tmpN.next!=head){
             if(tmpN.next.data==val){
                 tmpN.next=tmpN.next.next;
                 return true;
@@ -56,7 +56,7 @@ public class CircularLinkedList {
     public Node find(int val){
 
         Node tmpN=head;
-        while (tmpN.next!=null && tmpN.next!=head){
+        while (tmpN.next!=head){
             if (tmpN.data==val) {
                 return tmpN;
             }
@@ -69,7 +69,7 @@ public class CircularLinkedList {
     public void printfAll(){
         if(head==null)return;
         Node tmpN=head;
-        while (tmpN.next!=null && tmpN.next!=head){
+        while (tmpN.next!=head){
             System.out.print(tmpN.data+",");
             tmpN=tmpN.next;
         }
@@ -80,16 +80,16 @@ public class CircularLinkedList {
         CircularLinkedList cLinkedList=new CircularLinkedList();
          // 添加测试
         cLinkedList.add(10);
-        cLinkedList.add(11);
-        cLinkedList.add(12);
+//        cLinkedList.add(11);
+//        cLinkedList.add(12);
         cLinkedList.printfAll();
         // 删除测试
-//        boolean delete = cLinkedList.delete(10);
-//        cLinkedList.printfAll();
-//        System.out.println("删除："+delete);
+        boolean delete = cLinkedList.delete(10);
+        cLinkedList.printfAll();
+        System.out.println("删除："+delete);
         // 查找测试
-        Node node = cLinkedList.find(12);
-        System.out.println("查找："+( node==null ? null : node.data));
+//        Node node = cLinkedList.find(12);
+//        System.out.println("查找："+( node==null ? null : node.data));
     }
 
 }
