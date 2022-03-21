f045573a251e51d8c466b9a0655f26f122828846
sfqqyq <sfqqyq@163.com>
2022-03-21 21:26:24 +0800

    sfqqyq:插入排序

--- java/data-structure-and-algorithm/src/main/java/com/itsqq/sorts/Sorts.java
+++ java/data-structure-and-algorithm/src/main/java/com/itsqq/sorts/Sorts.java
@@ -53,11 +53,52 @@ public class Sorts {
         }
     }
 
+    // 插入排序
+    public static void insertionSort(int[] items){
+        int n=items.length;
+        if(n<=1)return;
+        for (int i = 1; i < n; i++) {
+            int value=items[i];
+            int j = i-1;
+            for (;j >=0; j--) {
+               if(items[j]>value){
+                items[j+1]=items[j];
+               } else {
+                   break;
+               }
+            }
+            items[j+1]=value;
+        }
+    }
+    // 插入排序练习2
+    public static void insertionSort2(int[] items){
+        int n=items.length;
+        if(n<=1)return;
+        // 以第二个元素为起始
+        for (int i = 1; i <n ; i++) {
+            int value=items[i];
+            // j从第一个元素开始
+            int j = i-1;
+            for (; j >=0 ; j--) {
+                // 从排过序部分倒叙比较，直至找到比items[i]值小的数据，
+                // 将value值赋值给items[j+1],j为比items[i]小的数据的下标，
+                // 所以是要放在j的后面的一个数据位
+                if(items[j]>value){
+                    items[j+1]=items[j];
+                }else {
+                    break;
+                }
+            }
+            items[j+1]=value;
+        }
+    }
+
     public static void main(String[] args) {
         // 逆序度=满有序度-有序度  满有序度：n(n-1)/2
         int[] items=new int[]{4,5,6,1,2,3};
 //        Sorts.bubbleSort(items);
-        Sorts.bubbleSort2(items);
+//        Sorts.bubbleSort2(items);
+        Sorts.insertionSort2(items);
         for (int i = 0; i < items.length; i++) {
             System.out.print(items[i]+",");
         }
